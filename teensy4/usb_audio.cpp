65ba417a9d55edfc2b238bee4f3b715350f7f984
Jake McGinty <me@jake.su>
2022-12-06 09:40:54 +0900

    Better documentation for USB descriptor (#4)
    better documentation for USB descriptor and usb_audio.*

--- teensy4/usb_audio.cpp
+++ teensy4/usb_audio.cpp
@@ -38,7 +38,11 @@
 bool AudioInputUSB::update_responsibility;
 audio_block_t * AudioInputUSB::incoming[AUDIO_CHANNELS];
 audio_block_t * AudioInputUSB::ready[AUDIO_CHANNELS];
-uint16_t AudioInputUSB::incoming_count;
+
+// The current amount that the incoming (pending) buffer is full. Between 0 and AUDIO_BLOCK_SAMPLES.
+uint16_t AudioInputUSB::incoming_count; 
+
+// A flag that there's data available for the next AudioStream consumer.
 uint8_t AudioInputUSB::receive_flag;
 
 struct usb_audio_features_struct AudioInputUSB::features = {0,0,FEATURE_MAX_VOLUME/2};
@@ -59,11 +63,16 @@ uint8_t usb_audio_transmit_setting=0;
 uint8_t usb_audio_sync_nbytes;
 uint8_t usb_audio_sync_rshift;
 
+// In the USB documentation:
+// Fs: The *actual* sample rate currently witnessed, as measured relative to the USB (micro)frames SOF.
+//     so, for Full-Speed that would be every 1ms, and for High-Speed every 125us (8x faster).
+// Ff: The *desired* data rate to achieve a target sample rate.
 uint32_t feedback_accumulator;
 
 volatile uint32_t usb_audio_underrun_count;
 volatile uint32_t usb_audio_overrun_count;
 
+volatile uint32_t sync_counter = 0, callback_counter = 0, samples_counted = 0;
 
 static void rx_event(transfer_t *t)
 {
@@ -74,14 +83,20 @@ static void rx_event(transfer_t *t)
 	}
 	usb_prepare_transfer(&rx_transfer, rx_buffer, AUDIO_RX_SIZE, 0);
 	arm_dcache_delete(&rx_buffer, AUDIO_RX_SIZE);
-	usb_receive(AUDIO_RX_ENDPOINT, &rx_transfer);
+	usb_receive(AUDIO_RX_EP, &rx_transfer);
 }
 
 static void sync_event(transfer_t *t)
 {
+	sync_counter++;
 	// USB 2.0 Specification, 5.12.4.2 Feedback, pages 73-75
-	//printf("sync %x\n", sync_transfer.status); // too slow, can't print this much
 	usb_audio_sync_feedback = feedback_accumulator >> usb_audio_sync_rshift;
+	// if (sync_counter % 4000 == 0) {
+	// 	char c[10];
+	// 	float feedback_float = (float)usb_audio_sync_feedback / (float)(1<<16);
+	// 	snprintf(c, 9, "%.6f", feedback_float);
+	// 	printf("sync %s\n", c); // too slow, can't print this much
+	// }
 	usb_prepare_transfer(&sync_transfer, &usb_audio_sync_feedback, usb_audio_sync_nbytes, 0);
 	arm_dcache_flush(&usb_audio_sync_feedback, usb_audio_sync_nbytes);
 	usb_transmit(AUDIO_SYNC_ENDPOINT, &sync_transfer);
@@ -92,7 +107,21 @@ void usb_audio_configure(void)
 	printf("usb_audio_configure\n");
 	usb_audio_underrun_count = 0;
 	usb_audio_overrun_count = 0;
+
+
+	// The feedback accumulator keeps track of how many samples have been seen
+	// based on the USB Host's clock, which for USB FS triggers every 1ms, and for
+	// USB HS triggers 8 times per 1ms (every 125us).
+	//
+	// We're multiplying by 2^24 here as a convenience for formatting the response
+	// to the host, which expects:
+	//   if High-Speed (480Mbps): An unsigned 10.10 fixed point binary, aligned as a 3-byte 10.14.
+	//   if Full-Speed ( 12Mbps): An unsigned 10.14 fixed point binary, aligned as a 4-byte 16.16.
+	// thus, the 
+	//
 	feedback_accumulator = (AUDIO_SAMPLE_RATE_EXACT / 1000.0f) * 0x1000000; // samples/millisecond * 2^24
+	// USB 2.0 High-Speed uses a 4-byte feedback format,
+	// where Full Speed uses a 3-byte format.
 	if (usb_high_speed) {
 		usb_audio_sync_nbytes = 4;
 		usb_audio_sync_rshift = 8;
@@ -101,13 +130,13 @@ void usb_audio_configure(void)
 		usb_audio_sync_rshift = 10;
 	}
 	memset(&rx_transfer, 0, sizeof(rx_transfer));
-	usb_config_rx_iso(AUDIO_RX_ENDPOINT, AUDIO_RX_SIZE, 1, rx_event);
+	usb_config_rx_iso(AUDIO_RX_EP, AUDIO_RX_SIZE, 1, rx_event);
 	rx_event(NULL);
 	memset(&sync_transfer, 0, sizeof(sync_transfer));
 	usb_config_tx_iso(AUDIO_SYNC_ENDPOINT, usb_audio_sync_nbytes, 1, sync_event);
 	sync_event(NULL);
 	memset(&tx_transfer, 0, sizeof(tx_transfer));
-	usb_config_tx_iso(AUDIO_TX_ENDPOINT, AUDIO_TX_SIZE, 1, tx_event);
+	usb_config_tx_iso(AUDIO_TX_EP, AUDIO_TX_SIZE, 1, tx_event);
 	tx_event(NULL);
 }
 
@@ -126,7 +155,7 @@ void AudioInputUSB::begin(void)
 	// means we no longer get receive callbacks from usb.c
 	update_responsibility = false;
 }
-static void copy_to_buffers_redux(const uint32_t *src, audio_block_t *chans[AUDIO_CHANNELS], unsigned int count, unsigned int len) 
+static void copy_to_buffers(const uint32_t *src, audio_block_t *chans[AUDIO_CHANNELS], unsigned int count, unsigned int len) 
 {
 	uint32_t *target = (uint32_t*) src + (len * AUDIO_CHANNELS/2);
 
@@ -137,8 +166,8 @@ static void copy_to_buffers_redux(const uint32_t *src, audio_block_t *chans[AUDI
 			chans[j*2]->data[count+i] = n & 0xFFFF;
 			// NOTE: inverting this sample here because for some reason
 			// one of the channels has a phase issue...
-			// chans[j*2+1]->data[count+i] = UINT16_MAX - (n >> 16);
-			chans[j*2+1]->data[count+i] = n >> 16;
+			chans[j*2+1]->data[count+i] = UINT16_MAX - (n >> 16);
+			// chans[j*2+1]->data[count+i] = n >> 16;
 		}
 		i++;
 	}
@@ -155,16 +184,34 @@ void usb_audio_receive_callback(unsigned int len)
 	const uint16_t *data;
 	const uint32_t *data_orig;
 
+
 	AudioInputUSB::receive_flag = 1;
-	len /= AUDIO_CHANNELS * AUDIO_SAMPLE_BYTES; // 1 sample = channels * bytes per sample
-	data = (const uint16_t *)rx_buffer;
+	// Let `len` now represent the number of "frames" of audio
+	// One frame includes exactly one sample per channel.
+	//
+	// For example, if we have 8 channels of 16-bit audio,
+	// one frame would be 16 bytes long.
+	len /= AUDIO_CHANNELS * AUDIO_SAMPLE_BYTES;
+	samples_counted += len;
+	callback_counter++;
+	// if (callback_counter % 12000 == 0) {
+	// 	char c[10];
+	// 	float average = (float)samples_counted / (float)callback_counter;
+	// 	snprintf(c, 9, "%.2f", average * 8.0f * 1000.0f);
+	// 	printf("%s\n", c);
+	// 	samples_counted = 0;
+	// 	callback_counter = 0;
+	// }
+
+	// A moving pointer to the USB receive buffer as we eat it up.
 	data_orig = (const uint32_t *)rx_buffer;
 
 	count = AudioInputUSB::incoming_count;
+
+	// Either use the existing incoming buffers or allocate new ones as needed.
 	for (uint32_t i = 0; i < AUDIO_CHANNELS; i++) {
 		chans[i] = AudioInputUSB::incoming[i];
 	}
-
 	for (uint32_t i = 0; i < AUDIO_CHANNELS; i++) {
 		if (AudioInputUSB::incoming[i] == NULL) {
 			chans[i] = AudioStream::allocate();
@@ -175,20 +222,31 @@ void usb_audio_receive_callback(unsigned int len)
 	while (len > 0) {
 		avail = AUDIO_BLOCK_SAMPLES - count;
 		if (len < avail) {
-			copy_to_buffers_redux(data_orig, chans, count, len);
+			// We can fit the entire incoming buffer in one AudioStream block,
+			// so we simply copy the whole thing in.
+			copy_to_buffers(data_orig, chans, count, len);
 			AudioInputUSB::incoming_count = count + len;
 			return;
 		} else if (avail > 0) {
-			copy_to_buffers_redux(data_orig, chans, count, avail);
-			data_orig += avail * AUDIO_CHANNELS/2;
-			data += avail * AUDIO_CHANNELS;
+			// Otherwise, we will be finishing up filling a block, and can
+			// flush it to the next consumer in the chain.
+			//
+			// We'll need to finish consuming the data after that, though,
+			// which is why this looks complicated.
+			copy_to_buffers(data_orig, chans, count, avail);
+			data_orig += avail * AUDIO_CHANNELS / AUDIO_SAMPLE_BYTES;
 			len -= avail;
 			for (int i = 0; i < AUDIO_CHANNELS; i++) {
 				if (AudioInputUSB::ready[i]) {
+					// If the previous ready buffers have not been consumed,
+					// all we can do is fill up our current incoming buffer
+					// and wait to be able to swap them.
 					AudioInputUSB::incoming_count = count + avail;
 					if (len > 0) {
+						// If there were remaining bytes of audio, they will
+						// be dropped because there is nowhere to put them.
 						usb_audio_overrun_count++;
-						printf("!");
+						printf("^ OVERRUN ^\n");
 						//serial_phex(len);
 					}
 					return;
@@ -245,20 +303,21 @@ void AudioInputUSB::update(void)
 	receive_flag = 0;
 	__enable_irq();
 	if (f) {
+		// Did we receive more or less than half 
 		int diff = AUDIO_BLOCK_SAMPLES/2 - (int)c;
 		feedback_accumulator += diff * 1;
 		//uint32_t feedback = (feedback_accumulator >> 8) + diff * 100;
 		//usb_audio_sync_feedback = feedback;
 
-		//printf(diff >= 0 ? "." : "^");
+		// printf(diff >= 0 ? "." : "^");
 	}
 	//serial_phex(c);
 	//serial_print(".");
 	for (int i = 0; i < AUDIO_CHANNELS; i++) {
 		if (!chans[i]) {
 			usb_audio_underrun_count++;
-			// printf("#"); // buffer underrun - PC sending too slow
-			if (f) feedback_accumulator += 3500;
+			printf("v UNDERRUN v\n"); // buffer underrun - PC sending too slow
+			// if (f) feedback_accumulator += 3500;
 			break;
 		}
 	}
@@ -295,7 +354,7 @@ static void tx_event(transfer_t *t)
 	usb_audio_sync_feedback = feedback_accumulator >> usb_audio_sync_rshift;
 	usb_prepare_transfer(&tx_transfer, usb_audio_transmit_buffer, len, 0);
 	arm_dcache_flush_delete(usb_audio_transmit_buffer, len);
-	usb_transmit(AUDIO_TX_ENDPOINT, &tx_transfer);
+	usb_transmit(AUDIO_TX_EP, &tx_transfer);
 }
 
 
